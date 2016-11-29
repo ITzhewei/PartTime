674a83214d12cf023f4a41dd3b77fe15b1ac46f6
john <1256618728@qq.com>
2016-11-29 15:43:11 +0800

    完成首页布局

--- app/src/main/java/com/zzw/john/parttime/model/index/IndexFragment.java
+++ app/src/main/java/com/zzw/john/parttime/model/index/IndexFragment.java
@@ -7,17 +7,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.youth.banner.Banner;
 import com.zzw.john.parttime.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by john on 2016/11/1.
  */
 
 public class IndexFragment extends Fragment {
+
+    //图片
+    Integer[] images = new Integer[]{
+            R.mipmap.h28,
+            R.mipmap.h29,
+            R.mipmap.h30
+    };
+    //设置图片标题
+    String[] titles = new String[]{
+            "h28",
+            "h29",
+            "h30",
+    };
+    @BindView(R.id.banner)
+    Banner mBanner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_index, null);
+        ButterKnife.bind(this,view);
+
+        mBanner.setBannerStyle(Banner.CIRCLE_INDICATOR);
+        mBanner.setIndicatorGravity(Banner.CENTER);
+        mBanner.isAutoPlay(true);
+        mBanner.setDelayTime(2000);
+        mBanner.setImages(images);
+
         return view;
     }
 }
