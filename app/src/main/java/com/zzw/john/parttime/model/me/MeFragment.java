ecb51df085ec2b91cfd8ca7ea96cd33c9a40d9b9
czr_83 <czr_83@163.com>
2016-11-30 19:37:19 +0800

    test

--- app/src/main/java/com/zzw/john/parttime/model/me/MeFragment.java
+++ app/src/main/java/com/zzw/john/parttime/model/me/MeFragment.java
@@ -18,7 +18,7 @@ public class MeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, null);
-        //12313
+        //123131313213
         return view;
     }
 }
